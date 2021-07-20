0d036ca229e0fe439fe128473b0277404ac37cca
Erik Larsson <elarsson@brave.com>
2021-07-20 13:19:53 +0200

    No segfault with atomic fn

--- plugin_api/src/allocator.rs
+++ plugin_api/src/allocator.rs
@@ -21,30 +21,31 @@ pub fn get_allocator() -> HostAllocatorPtr {
 pub mod host_alloctor {
     use super::*;
     use std::alloc::{GlobalAlloc, Layout};
-    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::atomic::Ordering;
 
-    pub unsafe fn set_allocator(allocator: HostAllocatorPtr) {
-        HOST_ALLOCATOR.alloc_fn.store(allocator.alloc_fn as usize, Ordering::SeqCst);
-        HOST_ALLOCATOR.dealloc_fn.store(allocator.dealloc_fn as usize, Ordering::SeqCst);
+    pub fn set_allocator(allocator: HostAllocatorPtr) {
+        HOST_ALLOCATOR.alloc_fn.store(Some(allocator.alloc_fn), Ordering::SeqCst);
+        HOST_ALLOCATOR.dealloc_fn.store(Some(allocator.dealloc_fn), Ordering::SeqCst);
     }
 
     struct HostAllocator {
-        alloc_fn: AtomicUsize,
-        dealloc_fn: AtomicUsize
+        pub alloc_fn: atomic::Atomic<Option<AllocFn>>,
+        pub dealloc_fn: atomic::Atomic<Option<DeallocFn>>,
     }
 
     #[global_allocator]
     static HOST_ALLOCATOR: HostAllocator = HostAllocator {
-        alloc_fn: AtomicUsize::new(0),
-        dealloc_fn: AtomicUsize::new(0),
+        alloc_fn: atomic::Atomic::new(None),
+        dealloc_fn: atomic::Atomic::new(None),
     };
 
     unsafe impl GlobalAlloc for HostAllocator {
         unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-            (std::mem::transmute::<_, AllocFn>(self.alloc_fn.load(Ordering::Relaxed)))(layout)
+            self.alloc_fn.load(Ordering::Relaxed).unwrap()(layout)
         }
+
         unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-            (std::mem::transmute::<_, DeallocFn>(self.dealloc_fn.load(Ordering::Relaxed)))(ptr, layout)
+            self.dealloc_fn.load(Ordering::Relaxed).unwrap()(ptr, layout)
         }
     }
 }
